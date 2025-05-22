a1f1ff67a7247cc6d360a83bc5fd552d962bb22c
Léo Guignard <leo.guignard@univ-amu.fr>
2025-05-22 16:52:14 +0200

    Refactor MoransI class to standalone functions and add comprehensive docstrings; create .gitignore for project dependencies

--- src/moransi/__init__.py
+++ src/moransi/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.0"
 
-from ._moransi import MoransI
+from ._moransi import morans_i_image, morans_i_adjacency_matrix
 
-__all__ = ["MoransI"]
+__all__ = ["morans_i_image", "morans_i_adjacency_matrix"]
